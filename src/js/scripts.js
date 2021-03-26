796099e9dc13c263c4e7dc5c47b51417840efb15
HannaHorbenko <annasgorbenko@gmail.com>
2021-03-26 22:31:04 +0200

    made slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -72,11 +72,12 @@
             infinite: true,
             autoplay: true,
             autoplaySpeed: 2000,
+            slidesToShow: 1,
             prevArrow: `<button class="slider__arrow slider__arrow--prev">
-                            <img src="../img/slider/arrow-right.png" alt="arrow-prev">                    
+                            <img src="img/slider/arrow-right.png" alt="arrow-prev">                    
                     </button>`,
             nextArrow: `<button class="slider__arrow slider__arrow--next">
-                            <img src="../img/slider/arrow-left.png" alt="arrow-next">
+                            <img src="img/slider/arrow-left.png" alt="arrow-next">
                     </button>`,
         });
     });
